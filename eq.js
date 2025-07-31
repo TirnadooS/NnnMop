e9ad29466f363102c4fb5eb39da06c411744b8d5
TirnadooS <savierzena@gmail.com>
2025-07-31 11:00:51 +0200

    Create eq.js

---
+++ eq.js
@@ -0,0 +1,13 @@
+const eqSettings = {
+    bass: 0,
+    mids: 0,
+    treble: 0
+};
+
+function initEqualizer() {
+    // Здесь будет реальная обработка через Web Audio API
+    console.log("Эквалайзер инициализирован");
+}
+
+// Инициализация при загрузке
+window.onload = initEqualizer;
