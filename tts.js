37f2a74fd682bf6142a8a669f4043ead6b456525
TirnadooS <savierzena@gmail.com>
2025-08-21 10:21:42 +0200

    Update tts.js

--- tts.js
+++ tts.js
@@ -1,6 +1,5 @@
-export function speak(text) {
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.lang = 'nb-NO'; // Норвежский Bokmål
-    utterance.rate = 0.9; // Скорость
-    window.speechSynthesis.speak(utterance);
+function speak(word) {
+  const utter = new SpeechSynthesisUtterance(word);
+  utter.lang = 'no-NO';
+  speechSynthesis.speak(utter);
 }
